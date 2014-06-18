9111e0b313f5ad99012034d51b6a6eec0e818677
Ben Kennedy <bgkennedy@gmail.com>
2014-06-18 09:17:55 +0100

    tweaks to options display js

--- app/assets/javascripts/options_display.js
+++ app/assets/javascripts/options_display.js
@@ -5,15 +5,17 @@ optionsDisplay.setupAccordion = function() {
   var currentGamesCount = parseInt(currentGamesCount);
   var invites = $('#pending_invites').attr('data_invites');
   var invitesCount = parseInt(invites);
-  if (currentGamesCount === 0 && invitesCount > 0 ) {
+  if (currentGamesCount === 0 && invitesCount === 0 ) {
+    $('#invite').addClass("active");    
     $('#current_games').removeClass("active");
-    $('#pending_invites').addClass("active");
-  } else if (currentGamesCount === 0) {
-    $('#invite').addClass("active");
-    $('#current_games').removeClass("active");
-  } else {
-
   }
+  if (currentGamesCount === 0) {
+    $('#current_games').removeClass("active");
+  }
+  if (invitesCount > 0 ) {
+    $('#pending_invites').addClass("active");
+    $('#current_games').removeClass("active");
+  } 
 }
 
 optionsDisplay.noticeTimeout = function(){
